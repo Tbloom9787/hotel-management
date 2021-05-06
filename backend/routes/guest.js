77cd200b65318654767466bee685b96330792dfe
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-05-05 17:33:04 -0700

    Add guest router for express connection

---
+++ backend/routes/guest.js
@@ -0,0 +1,10 @@
+const route = require('express').Router();
+
+const guestController = require('../controllers/guest');
+
+route.get('/', guestController.getAllGuests);
+route.get('/:id', guestController.getGuest);
+route.patch('/:id', guestController.updateGuest);
+route.post('/', guestController.addGuest);
+
+module.exports = route;
