1d11155e3728db404050d0ea6a2f900d80b3e97a
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-05-05 17:32:11 -0700

    Implemented controller for data logic of adding, updating, and retrieving a room

---
+++ backend/controllers/room.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Room = mongoose.model('Rooms');
+
+exports.getAllRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find({});
+    res.status(200).json(rooms);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+
+exports.getRoom = async (req, res) => {
+  try {
+    console.log(req.query);
+    const room = await Room.getRoombyId(req.query.roomid);
+    if (!room) {
+      throw new Error('Room not found');
+    }
+    res.status(200).json(room);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+
+exports.addRoom = async (req, res) => {
+  try {
+    const newRoom = new Room(req.body);
+    await newRoom.save();
+
+    res.status(201).json(newRoom);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
