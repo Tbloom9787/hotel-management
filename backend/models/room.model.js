4d7bbc5ac1d6d45841b696716ad978eb4ceddca8
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-05-07 23:14:14 -0700

    Refactor room model to refer having a guest object

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,56 +1,73 @@
 const mongoose = require('mongoose');
 
-const roomSchema = mongoose.Schema({
-  guestFirst: {
-    type: String,
-    required: false,
+const roomSchema = mongoose.Schema(
+  {
+    guestFirst: {
+      type: String,
+      required: false,
+    },
+    guestLast: {
+      type: String,
+      required: false,
+    },
+    dateReserved: {
+      type: String,
+      required: false,
+    },
+    checkIn: {
+      type: String,
+      required: false,
+    },
+    checkOut: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: false,
+    },
+    rate: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    paymentsMade: {
+      type: Number,
+      required: false,
+    },
+    balance: {
+      type: Number,
+      required: false
+    },
+    guest: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Guests',
+    },
   },
-  guestLast: {
-    type: String,
-    required: false,
-  },
-  dateReserved: {
-    type: String,
-    required: false,
-  },
-  checkIn: {
-    type: String,
-    required: false,
-  },
-  checkOut: {
-    type: String,
-    required: false,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  number: {
-    type: Number,
-    required: true,
-  },
-  website: {
-    type: String,
-    required: false,
-  },
-  rate: {
-    type: String,
-    required: true,
-  },
-  total: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-roomSchema.statics.getRoombyId = async function (roomid) {
+roomSchema.statics.getRoombyId = async function (id) {
   try {
     const room = await this.model('Rooms').findOne({
-      _id: roomid,
+      _id: id,
     });
 
     return room;
