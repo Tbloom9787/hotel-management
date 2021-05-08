3d01b6fced2be7b7930a31d54fce8c166438125b
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-05-07 23:13:37 -0700

    Refactor guest model to refer having a room object

--- backend/models/guest.model.js
+++ backend/models/guest.model.js
@@ -1,44 +1,50 @@
 const mongoose = require('mongoose');
 
-const guestSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const guestSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    stateID: {
+      type: String,
+      required: true,
+    },
+    licensePlate: {
+      type: String,
+      required: true,
+    },
+    room: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rooms' }],
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  stateID: {
-    type: String,
-    required: true,
-  },
-  licensePlate: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-guestSchema.statics.getGuestbyId = async function (guestid) {
+guestSchema.statics.getGuestbyId = async function (id) {
   try {
     const guest = await this.model('Guests').findOne({
-      _id: guestid,
+      _id: id,
     });
 
     return guest;
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
 };
