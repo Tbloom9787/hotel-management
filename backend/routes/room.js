0a50efdd653a71253f936d6ebdedd762950bce14
Tbloom9787 <Tylerbloom9787@gmail.com>
2021-05-07 23:14:39 -0700

    Fix route for query params

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -2,7 +2,7 @@ const route = require('express').Router();
 const roomController = require('../controllers/room');
 
 route.get('/', roomController.getAllRooms);
-route.get('/room', roomController.getRoom);
+route.get('/:id', roomController.getRoom);
 route.post('/addroom', roomController.addRoom);
 
 module.exports = route;
